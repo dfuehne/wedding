1416ae72d663c9a612d30e1e1da1f9a3bec722a3
dfuehne <duncan.fuehne@gmail.com>
2025-08-02 23:51:54 -0500

    fav icon surely will work now

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Zoe Baker and Duncan Fuehne Wedding Website!",
+    icons: {
+    icon: '/favicon.ico', // or '/logo.png'
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
