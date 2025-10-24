570d0b80b0a14bf31761e2166f2ceff9ad13db9f
lzoebaker <lzoebaker@gmail.com>
2025-10-24 09:31:59 -0500

    our story

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -50,7 +50,34 @@ export default function VenuePage() {
             </div>
         <div className="flex justify-center">
           <p className="text-xl font-medium">
-            We met in college.
+          Zoe was a sophomore at Colorado School of Mines competing on the cross country and track team when she met Duncan, a freshman joining the team.
+          </p>
+          <p>
+          Zoe thought Duncan was a little weird, and they didn't really talk their first year. Then, covid hit and no one saw each other much.
+          </p>
+          <p>
+          Right after the summer of 2020, Duncan slid back into Zoe's life through her Instagram DMs. Zoe decided that he was cute, and wanted to invite him to bake in her apartment (with help from her roommates Molly, Mel, and Alex).
+          </p>
+          <p>
+          For their first real date, they went together to a wine party at his apartment. Duncan tried to impress Zoe by setting up a table and showing her his *best* magic tricks. 
+          </p>
+          <p>
+          After a date to Buffalo Rose, a couple trips to the mountains, and lots of cross country practices, Zoe and Duncan were official that fall of 2021!
+          </p>
+          <p>
+          During the next few years of undergrad and grad school at Mines, they traveled around the country for competition and also took a few trips for fun (check out our Gallery page!)
+          </p>
+          <p>
+          After graduation, Zoe moved to San Francisco for a job and Duncan moved to UChicago to begin his Physics PhD. They made it work long distance for a year, with lots of weekend trips to see each other.
+          </p>
+          <p> 
+          In June of 2025 Duncan proposed to Zoe at a beautiful spot in the coastline of Catalina Island after a creating a massive scavenger hunt for her to solve, and supripsed her with family and friends! (Check out the Proposal page for more details!)
+          </p>
+          <p>
+          Serendipitously Zoe was able to secure an internal transfer to the Chicago office of her company soon after and they are excited to start this next chapter of their lives together in the same city!
+          </p>
+          <p>
+          We would love to celebrate this next chapter with you at our wedding!
           </p>
         </div>
     
