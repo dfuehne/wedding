6babc1890e83a353e0dbaa66aa2f636ae5b74776
dfuehne <duncan.fuehne@gmail.com>
2025-10-26 22:31:59 -0500

    title to our story

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -32,6 +32,7 @@ export default function VenuePage() {
       <Navbar/>
       <main className="pt-14 p-6">
           <div className="mx-auto place-self-center">
+
             {/* Logo */}
             <img
               src="logo.png"
@@ -39,6 +40,9 @@ export default function VenuePage() {
               className="mx-auto mb-6 w-32 h-auto"
             />
           </div>
+          <h1 className=" flex justify-center mb-4 max-w-2xl mx-auto text-2xl leading-none font-extrabold tracking-tight md:text-3xl xl:text-4xl">
+            Our Story
+          </h1>
             <div className="flex justify-center">
                 <Image
                 src={"https://firebasestorage.googleapis.com/v0/b/zdwedding.firebasestorage.app/o/story%2FIMG_0594.JPG?alt=media&token=8d601e3f-7ee5-46b2-aee2-771bbfebbeec"}
