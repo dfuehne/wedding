ce12600d2404309d735dca06b42514644b89423d
dfuehne <duncan.fuehne@gmail.com>
2025-10-26 22:27:14 -0500

    bug fix

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -53,6 +53,7 @@ export default function VenuePage() {
           Zoe was a sophomore at Colorado School of Mines competing on the cross country and track team when she met Duncan, a freshman joining the team.
           Zoe thought Duncan was a little weird, and they didn't really talk their first year. Then, covid hit and no one saw each other much.
           </p>
+          <p className="text-xl font-medium indent-8">
           Right after the summer of 2020, Duncan slid back into Zoe's life through her Instagram DMs. Two months later, Zoe finally liked message. Luckily Duncan tried again. Zoe decided that he was cute, and wanted to invite him to bake in her apartment (with help from her roommates Molly, Mel, and Alex).
           For their first real date, they went together to a wine party at his apartment. Duncan tried to impress Zoe by setting up a table and showing her his *best* magic tricks. 
           </p>
@@ -115,7 +116,7 @@ export default function VenuePage() {
                 width={400}
                 height={400}
                 />
-            </div>
+          </div>
         </div>
     
       </main>
